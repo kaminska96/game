2e7da1600cefe21924947c8163fdd7dfbd347959
zxc <yuliia.kaminska@gmail.com>
2023-05-22 23:35:02 +0300

    add game

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Score_Label = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Score_Label
+            // 
+            this.Score_Label.AutoSize = true;
+            this.Score_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Score_Label.Location = new System.Drawing.Point(12, 469);
+            this.Score_Label.Name = "Score_Label";
+            this.Score_Label.Size = new System.Drawing.Size(115, 29);
+            this.Score_Label.TabIndex = 0;
+            this.Score_Label.Text = "Score: 0";
+            this.Score_Label.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Score_Label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 2000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(747, 507);
+            this.Controls.Add(this.Score_Label);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label Score_Label;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        int score;
+        int count;
+        bool gameIsActive;
+        Random rand = new Random();
+
+        List<PictureBox> items = new List<PictureBox>();
+        //List<Rectangle> items = new List<Rectangle>();
+        public Form1()
+        {
+            InitializeComponent();
+            score = 0;
+            count = 0;
+
+            //RestartGame();
+        }
+
+        private void MakePictureBox()
+        {
+            PictureBox newPic = new PictureBox();
+            newPic.Height = 50;
+            newPic.Width = 50;
+            int color = rand.Next(1, 5);
+            switch (color) {
+                case 1:
+                    newPic.BackColor = Color.Red;
+                    break;
+                case 2:
+                    newPic.BackColor = Color.Yellow;
+                    break;
+                case 3:
+                    newPic.BackColor = Color.Blue;
+                    break;
+                case 4:
+                    newPic.BackColor = Color.Green;
+                    break;
+            }
+
+            int x = rand.Next(10, this.ClientSize.Width - newPic.Width);
+            int y = rand.Next(10, this.ClientSize.Height - newPic.Height);
+            newPic.Location = new Point(x, y);
+
+            newPic.Click += NewPicClick;
+            items.Add(newPic);
+            this.Controls.Add(newPic);
+            ++count;
+        }
+
+        private void NewPicClick(object sender, EventArgs e)
+        {
+            PictureBox tempPic = sender as PictureBox;
+
+            if (tempPic.BackColor == Color.Red) {
+                score += 10;
+            }
+            else if (tempPic.BackColor == Color.Yellow)
+            {
+                score += 5;
+            }
+            else if (tempPic.BackColor == Color.Blue)
+            {
+                score += 0;
+            }
+            else
+            {
+                score -= 20;
+            }
+            items.Remove(tempPic);
+            this.Controls.Remove(tempPic);
+            --count;
+
+            Score_Label.Text = "Score: " + score;
+
+        }
+
+        private void StartGame()
+        {
+            score = 0;
+            gameIsActive = true;
+        }
+        private void RestartGame()
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MakePictureBox();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (items.Count != 0)
+            {
+                items.RemoveAt(0);
+                this.Controls.Remove(items[0]);
+
+                if(items[0].BackColor != Color.Green)
+                {
+                    score -= 50;
+                }
+
+                --count;
+            }
+            Score_Label.Text = "Score: " + score;
+        }
+    }
+}
